a21bec3dbead7817258dd55f8152ef0e5be790b3
Matthew Ramos <mmramos309@gmail.com>
2020-01-13 11:06:56 -0500

    Created class for table view controller and set it to that class on storyboard

---
+++ UserDefaults-Demo/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  UserDefaults-Demo
+//
+//  Created by Matthew Ramos on 1/13/20.
+//  Copyright © 2020 Matthew Ramos. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
